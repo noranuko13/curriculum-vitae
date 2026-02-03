eaf3716e4fe0def3e048d74d400f90bbe72fe184
noranuko13 <noranuko13@gmail.com>
2026-02-03 12:22:59 +0900

    :notes: [Shell] Automate README.md TOC update

--- toc.sh
+++ toc.sh
@@ -13,9 +13,14 @@ patterns=(
 # * [アスタリスク]() から - [ハイフン]() への変換
 patterns+=("s/\*\s/- /")
 
-# コンソール出力
+# 目次置換
+TARGET_FILE="/curriculum-vitae/README.md"
 tmp=$(mktemp)
-./gh-md-toc --hide-header --hide-footer --indent=2 --start-depth=1 --depth=3 --no-escape /curriculum-vitae/README.md > "$tmp"
+./gh-md-toc --hide-header --hide-footer --indent=2 --start-depth=1 --depth=3 --no-escape "$TARGET_FILE" > "$tmp"
 (for f in "${patterns[@]}" ; do sed -ie "$f" "$tmp"; done)
-cat "$tmp"
+{
+  sed -n '1,/\[\/\/\]: # (TOC START)/p' "$TARGET_FILE"
+  cat "$tmp"
+  sed -n '/\[\/\/\]: # (TOC END)/,$p' "$TARGET_FILE"
+} > "${TARGET_FILE}.tmp" && mv "${TARGET_FILE}.tmp" "$TARGET_FILE"
 rm "$tmp"
